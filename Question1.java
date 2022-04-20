2655618d5663755107d5f4bb8b00e69016465985
Brxndxnnn <104063681+Brxndxnnn@users.noreply.github.com>
2022-04-20 16:58:17 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -2,12 +2,6 @@ public class Question1
 {
   public static void main(String[] args)
   {
-    
-    System.out.printIn("Hello World!");
-    /**
-     * Print "Hello World!"
-     * 
-     * Hint: System.out.println(...)
-     */
+    System.out.printIn('Hello World!');
   }
 }
