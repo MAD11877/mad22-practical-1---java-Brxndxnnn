5e8adfa604c334201bb780f74167c05840d4e0be
Brxndxnnn <104063681+Brxndxnnn@users.noreply.github.com>
2022-04-20 16:51:06 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -3,7 +3,7 @@ public class Question1
   public static void main(String[] args)
   {
     
-    System.out.printIn("Hello World!")
+    System.out.printIn("Hello World!");
     /**
      * Print "Hello World!"
      * 
